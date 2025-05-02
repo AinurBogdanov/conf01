2ffa5beb36561d960b1974398b73f7df74888e67
AinurWin <ainurbogdanov69@gmail.com>
2025-05-02 11:15:40 +0400

    create HTML for slide-menu

---
+++ src/scripts/InitSwiper.js
@@ -0,0 +1,36 @@
+import Swiper from 'swiper';
+import { Pagination, Autoplay } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination'
+
+
+export default function initSwiper() {
+  document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper-container', {
+    modules: [Pagination, Autoplay],
+    direction: 'horizontal',
+    loop:true,
+    touchRatio: 0.6,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction:false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: (index, className) => {
+        return `<span class="${className}">
+          <svg width="25" height="25" viewbox="0 0 40 40">
+            <circle class="white-circle" cx="20" cy="20" r="14" fill="none" stroke="
+            rgb(2, 2, 2)" stroke-width="2"/>
+
+            <circle class="progress-ring" cx="20" cy="20" r="12" fill="none" stroke="#EC9697" stroke-width="6" stroke-dasharray="101" stroke-dashoffset="100" />
+          </svg>
+        </span>
+        `
+      }
+    },
+  });
+  });
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,4 @@
-import Swiper from 'swiper';
-import { Pagination } from 'swiper/modules';
+import initSwiper from "./InitSwiper";
 
-import 'swiper/css/pagination'
-import 'swiper/css';
+initSwiper();
 
-document.addEventListener('DOMContentLoaded', () => {
-
-const swiper = new Swiper('.swiper-container', {
-  modules: [Pagination],
-  direction: 'horizontal',
-  loop:true,
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-});
-});
-console.log('scropt loaded')
